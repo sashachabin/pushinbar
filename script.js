64ed7e20d026b91b4542ff54c5e40732f5959b39
Alexander Chabin <nibach@ya.ru>
2021-12-24 04:21:03 +0500

    Вынес script.js в отдельный файл

---
+++ script.js
@@ -0,0 +1,24 @@
+(async () => {
+    const response = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=Qo3nCev3vKhzejCjIcVZhB3ULyuCcBbL96mT4beg5cEbpTrLIM9I2Vz2-MRljh3dZB7UVyrrKwBWI-HvYVs3EWLTaVdQp0jPm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCtUk43f48yQC-4h6uPTT3F5OK0fJemEGBaC-lLKqKzy2Q9eHLyJ9qux9rcQPyY6WCG_-W_z8TVH3c_8bZg2_Bdf-wvr4dxwbdz9Jw9Md8uu&lib=MAmgsdUMg_-ZrqH71iCQ13b_P0nMP0Yb0');
+    const data = await response.json();
+
+    const menu = document.getElementById('menu');
+
+    menu.innerHTML = data.map(({ brewery, enabled, beer, type1, type2, volume, price, cover  }) => {
+        const coverImage = cover || './images/cover-default.jpg';
+        return `
+        <article class="item">
+            <div class="item__cover-container">
+                <!-- <div class="item__type"><img src="image/${type1}" alt=""></div> -->
+                <!-- <div class="item__type">${type1}</div> -->
+
+                <img class="item__cover" class="" src="${coverImage}" alt="">
+                <div class="item__price">${price} ₽</div>
+            </div>
+                
+            <h3 class="item__beer">${beer}</h3>
+            <div class="item__volume">${volume}</div>
+        </article>
+    `;
+    }).join('');
+})();
